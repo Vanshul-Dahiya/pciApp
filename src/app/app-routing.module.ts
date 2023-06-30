665e3436223fc5148b243bff2228337a4846b85e
yahs3458 <73878897+yahs3458@users.noreply.github.com>
2023-06-30 13:16:08 +0530

    made css changes in login , splas,travelling,claim,table camera intigration and styling(ya)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PendingDashboardComponent } from './pending-dashboard/pending-dashboard.component';
 import { CommonModule } from '@angular/common';
+import { SplashscreenComponent } from './splashscreen/splashscreen.component';
+import { LoginComponent } from './login/login.component';
+import { GeneralinformationComponent } from './generalinformation/generalinformation.component';
+import { TableComponent } from './table/table.component';
+import { TravellingAllowanceComponent } from './travelling-allowance/travelling-allowance.component';
+import { ClaimCalculationComponent } from './claim-calculation/claim-calculation.component';
 
 const routes: Routes = [
+  {
+    component: SplashscreenComponent,
+    path: ''
+  },
+  {
+    component: LoginComponent,
+    path: 'login'
+  },
   {
     path : '', component:PendingDashboardComponent
   },
   {
     path : 'pendingDashboard', component:PendingDashboardComponent
-  }
+  },
+  { path: 'generalInfo', component: GeneralinformationComponent},
+  { path: 'table/:id', component: TableComponent},
+  { path: 'travelingAllowance', component: TravellingAllowanceComponent},
+  { path: 'calculation', component: ClaimCalculationComponent},
 ];
 
 @NgModule({
