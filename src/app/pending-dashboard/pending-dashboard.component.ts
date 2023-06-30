111a9b0ac63b58fd2391e392038664e9f8e00a44
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2023-06-30 16:29:34 +0530

    added routing to pending dashboard and changed bottom bar icon's color(v)

--- src/app/pending-dashboard/pending-dashboard.component.ts
+++ src/app/pending-dashboard/pending-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 // import {  OnInit, Output, EventEmitter  } from '@angular/core';
 
 @Component({
@@ -11,7 +12,24 @@ export class PendingDashboardComponent {
   // public onToggleSidenav = () => {
   //   this.sidenavToggle.emit();
   // }
+  constructor(private router :Router){}
   navigate(index : number){
     alert(`You clicked on ${index}`);
+    switch (index) {
+      case 1:
+        this.router.navigate(['/detail']);
+        break;
+      case 2:
+        this.router.navigate(['/report']);
+        break;
+      case 3:
+        this.router.navigate(['/pending']);
+        break;
+      case 4:
+        this.router.navigate(['/claimForm']);
+        break;
+      default:
+        break;
+    }
   }
 }
