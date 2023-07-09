e48fd6c0beeb15300e1e56042801aedad388a15d
yahs3458 <73878897+yahs3458@users.noreply.github.com>
2023-07-09 15:09:46 +0530

    nothing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'pci-app',
-  webDir: 'dist',
+  webDir:'www',
   server: {
     androidScheme: 'https'
   }
